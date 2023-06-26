43b0ccd441ebc2be7b5d13e27f6052752aba8c2b
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-06-26 14:59:29 +0200

    modified

--- script/medium.js
+++ script/medium.js
@@ -1,9 +1,15 @@
-
-const changeColorNav = document.getElementsByClassName('change-color') 
+console.log('hello')
+const navBar = document.querySelector('nav') 
+const navbutton = document.getElementsByClassName('background-button')
  document.addEventListener('scroll', function (e) {
     console.log(window.scrollY)
-    if (window.scrollY > 460) {
-      changeColorNav.classList.add('new-change-color')
+    if (window.scrollY > 360) {
+      navBar.classList.add('nav-color')
+      navbutton.classList.add('nav-button')
+      navBar.style.transition = '700ms'
+    } else {
+      navBar.classList.remove('nav-color')
+      navbutton.class.list.remove('nav-button')
     }
   })
   
